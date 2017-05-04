141ebfb61fad93106e23f43650450fa4e6f05c4e
Matis Masters <matis@craftedcode.com>
2017-05-04 03:56:32 -0300

    Refactor calculation to fix north-south direction

--- lib/lat_long/point.rb
+++ lib/lat_long/point.rb
@@ -20,20 +20,17 @@ module LatLong
       [@lat, @long]
     end
 
-    def move(distance, unit, direction)
-      @direction = direction_multipliers(direction)
-      @distance = distance_in_meters(distance, unit)
+    def move(raw_distance, unit, direction)
+      direction = direction_multipliers(direction)
+      distance = distance_in_meters(raw_distance, unit)
 
-      y_distance_to_move_in_meters = @distance * @direction[:y_axis]
-      x_distance_to_move_in_meters = @distance * @direction[:x_axis]
-
-      lat_offset = y_distance_to_move_in_meters / EARTH_RADIUS
-      long_offset = x_distance_to_move_in_meters / (
+      lat_offset = distance / EARTH_RADIUS
+      long_offset = distance / (
         EARTH_RADIUS * Math.cos(Math::PI * @lat/180)
       )
 
-      new_point_lat = @lat + lat_offset * 180/Math::PI
-      new_point_long = @long + long_offset * 180/Math::PI
+      new_point_lat = @lat + (lat_offset * 180/Math::PI) * direction[:y_axis]
+      new_point_long = @long + (long_offset * 180/Math::PI) * direction[:x_axis]
 
       { lat: new_point_lat, long: new_point_long }
     end
@@ -48,7 +45,7 @@ module LatLong
     def distance_in_meters(distance, unit)
       unit_multiplier = TO_METERS_MULTIPLIERS[unit] ||
         raise(ArgumentError, ARGUMENT_ERRORS_TEXT[:distance_unit])
-      distance * unit_multiplier
+      distance.to_f * unit_multiplier
     end
   end
 end
--- test/point/move_test.rb
+++ test/point/move_test.rb
@@ -3,7 +3,7 @@ class TestPointMove < Minitest::Test
   # something completely agnostic of formula or algorithm
   def setup
     # 500 meters between each point as per GMaps
-    @fst_and_17st_nw =            LatLong::Point.new(38.897356, (-77.039498))
+    @fst_and_17st_nw =            LatLong::Point.new(38.897343, (-77.039498))
     @washington_ave_and_17st_nw = LatLong::Point.new(38.892133, (-77.039498))
     @washington_ave_and_14st_nw = LatLong::Point.new(38.892133, (-77.033727))
   end
@@ -12,24 +12,28 @@ class TestPointMove < Minitest::Test
     moved_point = @washington_ave_and_17st_nw.move(500, :meters, :east)
 
     # Rounding to match GMaps precision
-    assert_equal moved_point[:lat].round(6), @washington_ave_and_14st_nw.lat
-    assert_equal moved_point[:long].round(6), @washington_ave_and_14st_nw.long
+    assert_equal @washington_ave_and_14st_nw.lat, moved_point[:lat].round(6)
+    assert_equal @washington_ave_and_14st_nw.long, moved_point[:long].round(6)
   end
 
   def test_point_is_moved_west_500_meters
     moved_point = @washington_ave_and_14st_nw.move(500, :meters, :west)
 
-    assert_equal moved_point[:lat].round(6), @washington_ave_and_17st_nw.lat
-    assert_equal moved_point[:long].round(6), @washington_ave_and_17st_nw.long
+    assert_equal @washington_ave_and_17st_nw.lat, moved_point[:lat].round(6)
+    assert_equal @washington_ave_and_17st_nw.long, moved_point[:long].round(6)
   end
 
+  def test_point_is_moved_north_580_meters
+    moved_point = @washington_ave_and_17st_nw.move(580, :meters, :north)
 
-  def test_point_is_moved_north_650_meters
-    # TODO: Not working for north and south
-    moved_point = @washington_ave_and_17st_nw.move(650, :meters, :north)
+    assert_equal @fst_and_17st_nw.lat, moved_point[:lat].round(6)
+    assert_equal @fst_and_17st_nw.long, moved_point[:long].round(6)
+  end
 
-    # Rounding to match GMaps precision
-    # assert_equal moved_point[:lat].round(6), @fst_and_17st_nw.lat
-    # assert_equal moved_point[:long].round(6), @fst_and_17st_nw.long
+  def test_point_is_moved_south_580_meters
+    moved_point = @fst_and_17st_nw.move(580, :meters, :south)
+
+    assert_equal @washington_ave_and_17st_nw.lat, moved_point[:lat].round(6)
+    assert_equal @washington_ave_and_17st_nw.long, moved_point[:long].round(6)
   end
 end
